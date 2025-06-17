574f62acb2cd5f593162d427adc3d0bcf701e24b
priyannshu08 <priyanshukumar83200@gmail.com>
2025-06-17 23:29:53 +0530

    The Second step with admin pages in it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,20 +9,33 @@ import MyBookings from './pages/MyBookings'
 import Favourite from './pages/Favourite'
 import { Toaster } from 'react-hot-toast'
 import Footer from './components/Footer'
+import Layout from './pages/Admin/Layout'
+import Dashboard from './pages/Admin/Dashboard'
+import AddShows from './pages/Admin/AddShows'
+import ListShows from './pages/Admin/ListShows'
+import ListBookings from './pages/Admin/ListBookings'
 const App = () => {
 
   const isAdminRoute = useLocation().pathname.startsWith('/admin')
 
   return (
     <>
+    {/* ✅ Toast notifications */}
+      <Toaster position="top-center" reverseOrder={false} />
       {!isAdminRoute && <Navbar />}
       <Routes>
         <Route path = '/' element={<Home/>} />
         <Route path = '/movies' element={<Movies/>} />
         <Route path = '/movies/:id' element={<MovieDetails/>} />
         <Route path = '/movies/:id/:date' element={<SeatLayout/>} />
-        <Route path = '/mybookings' element={<MyBookings/>} />
-        <Route path = '/favourite' element={<Favourite/>} />
+        <Route path = '/my-bookings' element={<MyBookings/>} />
+        <Route path = '/favorite' element={<Favourite/>} />
+        <Route path='/admin/*' element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="add-shows" element={<AddShows />} />
+          <Route path="list-shows" element={<ListShows />} />
+          <Route path="list-bookings" element={<ListBookings />} />
+        </Route>
       </Routes>
       {!isAdminRoute && <Footer />}
     </>
